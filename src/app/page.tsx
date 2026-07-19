f2392c8e156ac875be213ac1a716e186f6bbfb14
Pranshu <neil.morales0010@gmail.com>
2026-07-20 00:10:46 +0530

    remove: ResumeManager, ColdEmailComposer, SendOutreach components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,11 @@
 import React from 'react';
 import { Header } from '@/components/dashboard/Header';
 import { InternshipsTable } from '@/components/dashboard/InternshipsTable';
-import { ResumeManager } from '@/components/dashboard/ResumeManager';
-import { ColdEmailComposer } from '@/components/dashboard/ColdEmailComposer';
-import { SendOutreach } from '@/components/dashboard/SendOutreach';
 import { RecentActivity } from '@/components/dashboard/RecentActivity';
 import { AuthGuard } from '@/components/dashboard/AuthGuard';
 import { StatsCards } from '@/components/dashboard/StatsCards';
 
 export default function DashboardPage() {
-  const [resumeFileName, setResumeFileName] = React.useState<string | null>(null);
-  const [emailSubject, setEmailSubject] = React.useState<string>('');
-  const [emailBody, setEmailBody] = React.useState<string>('');
-
   return (
     <div className="min-h-screen bg-background flex flex-col font-sans">
       <Header />
@@ -30,20 +23,6 @@ export default function DashboardPage() {
           </div>
 
           <InternshipsTable />
-
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <ResumeManager onResumeUpdate={setResumeFileName} />
-            <ColdEmailComposer onTemplateUpdate={(subject, body) => {
-              setEmailSubject(subject);
-              setEmailBody(body);
-            }} />
-          </div>
-
-          <SendOutreach
-            resumeFileName={resumeFileName}
-            emailSubject={emailSubject}
-            emailBody={emailBody}
-          />
 
           <RecentActivity />
         </AuthGuard>
